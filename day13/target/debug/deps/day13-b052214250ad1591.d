2f8c31971406d63505354dec009ee5394ea6de0c
Huy <kingbazoka@gmail.com>
2022-12-14 00:10:04 +0000

    Remove cache

--- day13/target/debug/deps/day13-b052214250ad1591.d
+++
@@ -1,6 +0,0 @@
-/workspaces/aoc-2022/day13/target/debug/deps/day13-b052214250ad1591.rmeta: src/main.rs src/../input.txt
-
-/workspaces/aoc-2022/day13/target/debug/deps/day13-b052214250ad1591.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- day13/target/debug/deps/day13-bb7f76352a9cea0b.d
+++
@@ -1,6 +0,0 @@
-/workspaces/aoc-2022/day13/target/debug/deps/day13-bb7f76352a9cea0b.rmeta: src/main.rs src/../input.txt
-
-/workspaces/aoc-2022/day13/target/debug/deps/day13-bb7f76352a9cea0b.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
